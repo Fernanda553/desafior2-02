1856fb76b0546fae2511c0765b6b9064e724fb08
fernanda nuñez <fernanda.nunez55@gmail.com>
2023-07-04 17:02:22 -0400

    add view NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./views/Home";
 import Contacto from "./views/Contacto";
+import NotFound from "./views/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contacto" element={<Contacto />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/views/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const NotFound = () => {
+  return (
+    <Container className="pt-5">
+      <img src="../public/notfound3.png" alt="Not found" />
+    </Container>
+  );
+};
+
+export default NotFound;
